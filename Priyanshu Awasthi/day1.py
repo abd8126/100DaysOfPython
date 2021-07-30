3bd8694d6a1aadefa66071b72e1db087e22cf203
priyanshuawas <priyanshu.awas011@gmail.com>
2021-07-30 19:43:14 +0530

    Day2

--- Priyanshu Awasthi/day1.py
+++ Priyanshu Awasthi/day1.py
@@ -1,4 +1,4 @@
 print("Welcome to the band name generator.")
 city = input("Which city did you grow up in?")
-pet = input("what is the name of a pet");
+pet = input("what is the name of a pet")    # Remove Semicolon from the 3rd line 
 print("Your band name could be " + city + " " + pet)
---
+++ Priyanshu Awasthi/day2.py
@@ -0,0 +1,4 @@
+a= float(input())
+if a == 150:
+    totalbill=(a/5)*1.12
+print("%0.2f"%totalbill)
