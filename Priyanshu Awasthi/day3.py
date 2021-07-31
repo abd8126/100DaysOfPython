4176c0e487a63c0a87374a390132389425985045
priyanshuawas <priyanshu.awas011@gmail.com>
2021-07-31 15:11:31 +0530

    some changes in Day3

--- Priyanshu Awasthi/day3.py
+++ Priyanshu Awasthi/day3.py
@@ -1,5 +1,5 @@
-weight = input("Enter the weight in kilogram :")
-height = input("Enter the height in centimeters :")
+weight = float(input("Enter the weight in kilogram :"))
+height = float(input("Enter the height in centimeters :"))
 BMI= weight/ (height/100)**2
 if BMI < 18.5 :
     print("You are underweight")
@@ -10,4 +10,4 @@ elif BMI >= 25 and BMI < 30 :
 elif BMI >= 30 and BMI < 35 :
     print("You are Obese")
 elif BMI >= 35 :
-    print("You are clinically obese ")
+    print("You are clinically obese")
