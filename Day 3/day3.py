66d23a24d4c5acb3046897590dbb5dcd91fe751f
Kshitiz Kumar Singh <kshitiz.singh_cs20@gla.ac.in>
2021-07-31 12:28:05 +0530

    BMI Calculator

---
+++ Day 3/day3.py
@@ -0,0 +1,18 @@
+height = input("Enter height in metres : ")
+weight = input("Enter weight in kgs : ")
+
+try:
+    bmi = round(float(weight)/(float(height)**2))
+
+    if bmi < 18.5:
+        print(f"Your BMI is {bmi} and you are underweight.")
+    elif bmi < 25:
+        print(f"Your BMI is {bmi} and you are normal.")
+    elif bmi < 30:
+        print(f"Your BMI is {bmi} and you are slightly overweight.")
+    elif bmi < 35:
+        print(f"Your BMI is {bmi} and you are obese.")
+    else:
+        print(f"Your BMI is {bmi} and you are clinically obese.")
+except ValueError:
+    print("Enter integer or float values only.")
