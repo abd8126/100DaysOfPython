b2787e031ede7eb2a5487c9743fa9099a43afefb
Santosh Kumar Morya <santoshmorya400@gmail.com>
2021-07-31 18:50:59 +0530

    Day 3

--- Santosh Morya1/Day3.py
+++ Santosh Morya1/Day3.py
@@ -1,14 +1,15 @@
 
 Weight = float(input("Weight in KG: "))
 height = float(input("height in meter: "))
-BMI_Index=Weight/height**2
-if(BMI_Index < 18.5):
+BMI=Weight/height**2
+if(BMI < 18.5):
     print("you are underweight")
-elif(BMI_Index<25):
+elif(BMI<25):
      print("Your have normal weight")
-elif(BMI_Index < 30):
+elif(BMI < 30):
     print("you are below overweight")
-elif(BMI_Index < 35):
+elif(BMI < 35):
     print("you are overweight, Please do excersice and other stuff")
-elif(BMI_Index > 35):
+elif(BMI > 35):
     print("You are clinically obese")
+print(BMI)
