ce31287fd98ca86d1184a87a9b4cc24821ac98a6
Santosh Kumar Morya <santoshmorya400@gmail.com>
2021-08-02 21:15:06 +0530

    Random password generator

--- Santosh Morya1/Day5.py
+++ Santosh Morya1/Day5.py
@@ -2,8 +2,7 @@ import random
 import string
 
 
-length= int(input("Enter the length of the password"))
+length= int(input("Enter the length of the password "))
 collection = string.ascii_lowercase +string.ascii_letters+string.ascii_uppercase+string.punctuation
 password = ''.join(random.choice(collection) for i in range(length))
-print(f"Random string of length {length} is {password}")
-
+print(f"Random password of length {length} is {password}")
