55a8d322e58bf8ac8cd54190b083402aa2107a72
kalyaniag <87924570+kalyaniag@users.noreply.github.com>
2021-08-09 15:36:47 +0530

    DAY 1

---
+++ #day 1.py
@@ -0,0 +1,7 @@
+#day 1
+
+print("Welcome to Band name gernator")
+city=input("Name the city where you grew up")
+pet=input("Name of the Pet")
+Band_name= city + " " + pet
+print("Band name is",Band_name)
