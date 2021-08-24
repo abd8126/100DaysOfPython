74d0b04236385b36167910ff55c45196927b0c4e
shubhigupta2604 <gshubhi2604@gmail.com>
2021-08-25 01:20:49 +0530

    day8

---
+++ Shubhi Gupta/day8.py
@@ -0,0 +1,10 @@
+h=int(input("Enter the height of wall in meter:- "))
+w=int(input("Enter the width of wall in meter:- "))
+area= h*w
+y= area/5                    # 1 can cover 5sq meter
+x= round(y)
+# we want everytime can should be sufficent to color
+if(y>x):
+    print(x+1)               
+else:
+    print(x)
