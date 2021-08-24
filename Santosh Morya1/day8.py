3da8e27e51831d4337566def2c7e1a5b3f33d518
Santosh Kumar Morya <santoshmorya400@gmail.com>
2021-08-24 20:14:11 +0530

    day8 solution

---
+++ Santosh Morya1/day8.py
@@ -0,0 +1,8 @@
+h = eval(input("Enter the height of wall:"))
+w = eval(input("enter the width of wall: "))
+
+cans_require = round((h*w)/5)
+if cans_require == 1:
+    print("number of can required is: ",cans_require)
+else:
+    print("number of cans required are: ",cans_require)
