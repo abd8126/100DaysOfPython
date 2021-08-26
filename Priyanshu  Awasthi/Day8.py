655b0f974267f3db53fa8d68e3e38445b2449b20
priyanshuawas <priyanshu.awas011@gmail.com>
2021-08-26 12:03:23 +0530

    Day 8

---
+++ Priyanshu  Awasthi/Day8.py
@@ -0,0 +1,6 @@
+height=int(input("enter the height of the wall "))
+width=int(input("enter the width of the wall "))
+totalcoverage=int(input("enter the coverage per pan  "))
+cans_reqired=round(height*width/totalcoverage)
+print("Total cans required to paint the wall are  : " ,cans_reqired) 
+
