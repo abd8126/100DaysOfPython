d79b4bcb760ec2bab938dd3996afacb3c6f831ac
MADHAV MAHESHWARI <87849042+madhav-293@users.noreply.github.com>
2021-09-20 21:56:56 +0530

    Day-10

---
+++ Madhav/Day10.4.py
@@ -0,0 +1,6 @@
+#PROGRAM TO CHECK GIVEN INTEGER IS MULTIPLE OF 5 ND 7 OR NOT
+num=int(input("ENTER THE NUMBER :"))
+if num%5==0 and num%7==0:
+    print("NUM IS MULTIPLE OF 5 AND 7")
+else:
+    print("NUM IS NOT MULTIPLE OF 5 AND 7")
